d5c25cb317c7659e27ca865083aa2d7c94bf7b10
xU <142771851+xUankip@users.noreply.github.com>
2023-11-16 21:31:12 +0700

    src react

--- src/App.js
+++ src/App.js
@@ -33,20 +33,33 @@ function  App() {
   }
 // export default App;
 // import './App.css';
-function UserProfile(){
-    return(
-      <div>
-      <h1>Xuaan</h1>
-  <img src="http://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-   className="user-profile-pic" 
-   alt="Mark"/>
-      <h2>Hobbiees</h2>
-      <ul>
-          <li>Badminton</li>
-          <li>chess</li>
-          <li>Gaming</li>
-      </ul>
-      </div>
-    )
-  }
-  export default UserProfile;
+// function UserProfile(){
+//     return(
+//       <div>
+//       <h1>Xuaan</h1>
+//   <img src="http://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+//    className="user-profile-pic" 
+//    alt="Mark"/>
+//       <h2>Hobbiees</h2>
+//       <ul>
+//           <li>Badminton</li>
+//           <li>chess</li>
+//           <li>Gaming</li>
+//       </ul>
+//       </div>
+//     )
+//   }
+//   export default UserProfile;
+function User(){
+  const name= "Xuann"
+  const pic = "http://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  return (
+    <section>
+      <img className='user-profile-pic' src={pic} alt={name} />
+      <h2 className='user-name'>
+        Hello, <span className='user-first-name'>{name}</span> 🥰
+      </h2>
+    </section>
+  )
+}
+export default User;
--- src/App2.js
+++ src/App2.js
@@ -1,14 +1,56 @@
 import "./App.css"
-function App2(){
-    return( <div className="App2">
-        <Welcome2 name="Neooo" />
-    </div>);
-}
-function Welcome2(props){
+// function App2(){
+//     return( <div className="App2">
+//         <Welcome2 name="Neooo" />
+//     </div>);
+// }
+// function Welcome2(props){
+//     return (
+//         <section>
+//             <h1>Hello, <span>{props.name}</span></h1>
+//         </section>
+//     )
+// }
+// export default App2;
+function User({name, pic}){
     return (
         <section>
-            <h1>Hello, <span>{props.name}</span></h1>
+            <img className="user-profile-pic" src={pic} alt={name} />
+            <h2 className="user-name">
+                Hello, <span className="user-first-name">{name}</span>
+            </h2>
         </section>
     )
 }
-export default App2;
+function App(){
+    // const names = ["Cuban", "Biden", "Donnal", "Apieceofshit"];
+    // return (
+    //     <div className="App">
+    //         {names.map((nm) =>(<User name= {nm} />))}
+    //     </div>
+    // )
+    const users = [{
+        name: "Cuban",
+        pic : "https://images.unsplash.com/photo-1618641986557-1ecd230959aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFY2h8NXx8cHJvZmkszXxlbnwwfHwwfHw%3D&w=1000&q=80"
+    },
+    {
+        name : "Biden",
+        pic : "https://images.unsplash.com/photo-1618641986557-1ecd230959aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFY2h8NXx8cHJvZmkszXxlbnwwfHwwfHw%3D&w=1000&q=80"
+    },
+       { name : "Donnal",
+        pic : "https://images.unsplash.com/photo-1618641986557-1ecd230959aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFY2h8NXx8cHJvZmkszXxlbnwwfHwwfHw%3D&w=1000&q=80"
+       }
+       ,{
+        name : "Apieceofshit",
+        pic : "https://images.unsplash.com/photo-1618641986557-1ecd230959aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFY2h8NXx8cHJvZmkszXxlbnwwfHwwfHw%3D&w=1000&q=80"
+       }
+    ]
+    return (
+        <div className="App">
+            {users.map((user) => (
+                <User name={user.name} pic={user.pic} />
+            ))}
+        </div>
+    )
+}
+export default App;
